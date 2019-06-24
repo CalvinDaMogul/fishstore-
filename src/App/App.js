bbca50534e2396c20677a41ffd6fe5019831b369
Calvin J Foster <cal.j.foster@gmail.com>
2019-06-24 18:52:53 -0500

    setup done

---
+++ src/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
+
+class App extends React.Component {
+  render() {
+    return (
+    <div className="App">
+       <Auth />
+       <Home />
+    </div>
+    );
+  }
+}
+
+export default App;
