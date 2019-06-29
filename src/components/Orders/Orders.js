e913e63a62ad130fc70e86d7465a651a81703aa2
Calvin J Foster <cal.j.foster@gmail.com>
2019-06-29 16:34:48 -0500

    added delete order

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,33 +1,30 @@
+/* eslint-disable eol-last */
 import React from 'react';
-import firebase from 'firebase/app';
 import 'firebase/auth';
+import PropTypes from 'prop-types';
 
-import ordersData from '../../helpers/data/ordersData';
+import orderShapes from '../../helpers/propz/orderShapes';
+
 import OrderRow from '../OrderRow/OrderRow';
 
 class Orders extends React.Component {
-    state = {
-      orders: [],
-    }
+  static propTypes = {
+    orders: PropTypes.arrayOf(orderShapes.orderShapes),
+    deleteOrder: PropTypes.func.isRequired,
+  }
 
-    componentDidMount() {
-      ordersData.getMyOrders(firebase.auth().currentUser.uid)
-        .then(orders => this.setState({ orders }))
-        .catch(err => console.error('cant get orders', err));
-    }
+  render() {
+    const orderComponents = this.props.orders.map(order => (
+      <OrderRow key={order.id} order={order} deleteOrder={this.props.deleteOrder}/>
+    ));
 
-    render() {
-      const orderComponents = this.state.orders.map(order => (
-          <OrderRow key={order.id} order={order} />
-      ));
-
-      return (
-        <div className="Orders">
+    return (
+      <div className="Orders">
         <h2>Orders</h2>
         <table className="table">
           <thead>
             <tr>
-              <th scope="col">Order #</th>
+              <th scope="col">Order Name</th>
               <th scope="col">Date</th>
               <th scope="col"># Fish</th>
               <th scope="col"></th>
@@ -38,8 +35,8 @@ class Orders extends React.Component {
           </tbody>
         </table>
       </div>
-      );
-    }
+    );
+  }
 }
 
 export default Orders;
